c7f8d842e571a68899c6324558742b4151b89f2b
felipendelicia <delicia4581@gmail.com>
2022-05-08 23:05:08 -0300

    feat: players component and started the search bar

--- src/components/General/Players/Players.tsx
+++ src/components/General/Players/Players.tsx
@@ -1,21 +1,54 @@
 import React from 'react'
+import {Link} from "react-router-dom"
 
 import "./Players.css"
 
+import SearchBar from './SearchBar/SearchBar'
+
 import { players } from '../../../data/players'
+import { goals } from '../../../data/goals'
+import { teams } from '../../../data/teams'
 
 const Players = () => {
-  return (
-    <div className="players-container">
-        <div className="search-bar-container">
 
+  const handlePlayerGoals = (id:string) => {
+    let playerGoals = 0
+    for(let i = 0;i<goals.length;i++){
+      if(goals[i].playerGoalId===id){
+        playerGoals = playerGoals + 1
+      }
+    }
+    return playerGoals
+  }
+
+  return (
+    <div className="players-container-main">
+        <div className="search-bar-container">
+          <SearchBar/>
         </div>
         <div className="players-container">
           {
             players.map((player, index)=>{
+              let currentTeam = ""
+              for(let i = 0;i<teams.length;i++){
+                if(teams[i].id===player.teamId){
+                  currentTeam = teams[i].name.replaceAll(" ", "")
+                  break
+                }
+              }
+
               return(
                 <div className="player-container" key={index}>
-                  {player.name}
+                  <div className="player-photo-name">
+                    <img src="./assets/img/people.png" alt="playerPic" className="player-photo"/>
+                    <p className="player-name">{player.name}</p>
+                  </div>
+                  <div className="player-goals-team">
+                    <p>{handlePlayerGoals(player.id)}</p>
+                    <Link to={"/team/" + currentTeam}>
+                      <img src="./assets/img/favicon.png" className="player-photo" alt="team shield"/>
+                    </Link>
+                  </div>
                 </div>
               )
             })
---
+++ src/components/General/Players/SearchBar/SearchBar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import "./SearchBar.css"
+
+const SearchBar = () => {
+  return (
+    <div className="searchbar-container">
+        <input placeholder="Buscar jugador"></input>
+    </div>
+  )
+}
+
+export default SearchBar
