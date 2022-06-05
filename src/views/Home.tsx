7eb86a01b22ee86c3b274c678f4e29b64c5e8e21
felipendelicia <delicia4581@gmail.com>
2022-06-05 20:39:31 -0300

    feat: tournament stages

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Carrousel from '../components/Carrousel/Carrousel'
 import Fixture from '../components/Fixture/Fixture'
 import BestScorers from '../components/BestScorers/BestScorers'
+import Tournament from '../components/Tournament/Tournament'
 
 const Home = () => {
   return (
     <div className="container">
         <Carrousel/>
         <Fixture/>
+        <Tournament/>
         <BestScorers/>
     </div>
   )
